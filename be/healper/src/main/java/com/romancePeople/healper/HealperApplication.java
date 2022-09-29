1f4f715db0412693631e3ceda20651acf59d0c47
kssumin <201566@jnu.ac.kr>
2022-09-29 18:53:03 +0900

    feat : Initial setting of development environment

---
+++ be/healper/src/main/java/com/romancePeople/healper/HealperApplication.java
@@ -0,0 +1,13 @@
+package com.romancePeople.healper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealperApplication.class, args);
+	}
+
+}
---
+++ be/healper/src/test/java/com/romancePeople/healper/HealperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romancePeople.healper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
